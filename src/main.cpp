09e81df2581f7e3bbdc0bcc937da5021ba790ed8
boristestov <boris.testov@gmail.com>
2020-09-03 00:43:42 +0300

    Main code added
     On branch master
     Changes to be committed:
    	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "mvc.h"
+#include "primitives.h"
 #include "version.h"
 
 #include <iostream>
@@ -6,6 +8,18 @@
 
 int main()
 {
+    // TODO: Добавить описания к методам
+    // TODO: Добавить тесты
     std::cout << "Current version: " << version() << std::endl;
+    Model model;
+    View view;
+    Controller controller(model, view);
+    model.setController(controller);
+
+    controller.createNewDocument();
+    controller.addPrimitive(std::make_unique<Circle>(100, 50, 20));
+    controller.addPrimitive(std::make_unique<Rectangle>(0, 10, 20, 80));
+    model.refresh();
+    controller.exportToFile("testfile.bin");
     return 0;
 }
